de4484d146711395d464b3445d8d5496770e8477
Christiane Ruetten <cr@23bit.net>
2011-05-23 11:38:05 +0200

    reformatting tabs

--- driver/display.h
+++ driver/display.h
@@ -78,33 +78,33 @@ typedef union
 {
   struct
   {
-  	// Line1 + Line2 + Icons
-    u16 full_update      		: 1;    // 1 = Redraw all content
-    u16 partial_update      	: 1;    // 1 = Update changes
+    // Line1 + Line2 + Icons
+    u16 full_update                     : 1;	// 1 = Redraw all content
+    u16 partial_update                  : 1;	// 1 = Update changes
   	
-  	// Line only
-    u16 line1_full_update     	: 1;    // 1 = Redraw Line1 content
-    u16 line2_full_update     	: 1;    // 1 = Redraw Line2 content
+    // Line only
+    u16 line1_full_update               : 1;	// 1 = Redraw Line1 content
+    u16 line2_full_update               : 1;	// 1 = Redraw Line2 content
 
-	// Logic module data update flags
-    u16 update_time      		: 1;    // 1 = Time was updated 
-#ifdef CONFIG_SIDEREAL
-	u16 update_sidereal_time	: 1;	// 1 = Sidereal Time was updated
-#endif
-    u16 update_stopwatch     	: 1;    // 1 = Stopwatch was updated
-#ifdef CONFIG_EGGTIMER
-    u16 update_eggtimer : 1;
-#endif
-    u16 update_temperature   	: 1;    // 1 = Temperature was updated
-    u16 update_battery_voltage 	: 1;    // 1 = Battery voltage was updated
-    u16 update_date      		: 1;    // 1 = Date was updated
-#ifdef CONFIG_CYCLE_ALARM
-    u16 update_cycle_alarm     		: 1;    // 1 = Cycle Alarm was updated
-#endif
-#ifdef CONFIG_ALARM
-    u16 update_alarm      		: 1;    // 1 = Alarm time was updated
-#endif
-    u16 update_acceleration		: 1; 	// 1 = Acceleration data was updated
+    // Logic module data update flags
+    u16 update_time      		: 1;	// 1 = Time was updated 
+    #ifdef CONFIG_SIDEREAL
+    u16 update_sidereal_time            : 1;	// 1 = Sidereal Time was updated
+    #endif
+    u16 update_stopwatch                : 1;	// 1 = Stopwatch was updated
+    #ifdef CONFIG_EGGTIMER
+    u16 update_eggtimer                 : 1;
+    #endif
+    u16 update_temperature              : 1;	// 1 = Temperature was updated
+    u16 update_battery_voltage          : 1;	// 1 = Battery voltage was updated
+    u16 update_date                     : 1;	// 1 = Date was updated
+    #ifdef CONFIG_CYCLE_ALARM
+    u16 update_cycle_alarm     		: 1;	// 1 = Cycle Alarm was updated
+    #endif
+    #ifdef CONFIG_ALARM
+    u16 update_alarm                    : 1;	// 1 = Alarm time was updated
+    #endif
+    u16 update_acceleration             : 1;	// 1 = Acceleration data was updated
   } flag;
   u16 all_flags;            // Shortcut to all display flags (for reset)
 } s_display_flags;
@@ -118,7 +118,7 @@ extern volatile s_display_flags display;
 // Display function modes
 #define DISPLAY_LINE_UPDATE_FULL		(BIT0)
 #define DISPLAY_LINE_UPDATE_PARTIAL		(BIT1)
-#define DISPLAY_LINE_CLEAR				(BIT2)
+#define DISPLAY_LINE_CLEAR			(BIT2)
 
 // Definitions for line view style
 #define DISPLAY_DEFAULT_VIEW			(0u)
@@ -126,12 +126,12 @@ extern volatile s_display_flags display;
 #define DISPLAY_ALTERNATIVE2_VIEW		(2u)
  
 // Definitions for line access
-#define LINE1							(1u)
-#define LINE2							(2u)
+#define LINE1					(1u)
+#define LINE2					(2u)
 
 // LCD display modes
-#define SEG_OFF					(0u)
-#define	SEG_ON					(1u)
+#define SEG_OFF				(0u)
+#define	SEG_ON				(1u)
 #define SEG_ON_BLINK_ON			(2u)
 #define SEG_ON_BLINK_OFF		(3u)
 #define SEG_OFF_BLINK_OFF		(4u)
@@ -156,8 +156,8 @@ extern volatile s_display_flags display;
 // xxx_L2_xxx 		= Item is part of Line2 information
 
 // Symbols for Line1
-#define LCD_SYMB_AM					0
-#define LCD_SYMB_PM					1
+#define LCD_SYMB_AM				0
+#define LCD_SYMB_PM				1
 #define LCD_SYMB_ARROW_UP			2
 #define LCD_SYMB_ARROW_DOWN			3
 #define LCD_SYMB_PERCENT			4
@@ -266,30 +266,30 @@ extern volatile s_display_flags display;
 #define LCD_SEG_L2_DP_MEM			(LCD_MEM_9)
 #define LCD_SYMB_AM_MEM				(LCD_MEM_1)
 #define LCD_SYMB_PM_MEM				(LCD_MEM_1)
-#define LCD_SYMB_ARROW_UP_MEM		(LCD_MEM_1)
-#define LCD_SYMB_ARROW_DOWN_MEM		(LCD_MEM_1)
-#define LCD_SYMB_PERCENT_MEM		(LCD_MEM_5)
+#define LCD_SYMB_ARROW_UP_MEM			(LCD_MEM_1)
+#define LCD_SYMB_ARROW_DOWN_MEM			(LCD_MEM_1)
+#define LCD_SYMB_PERCENT_MEM			(LCD_MEM_5)
 #define LCD_SYMB_TOTAL_MEM			(LCD_MEM_11)
-#define LCD_SYMB_AVERAGE_MEM		(LCD_MEM_10)
+#define LCD_SYMB_AVERAGE_MEM			(LCD_MEM_10)
 #define LCD_SYMB_MAX_MEM			(LCD_MEM_8)
-#define LCD_SYMB_BATTERY_MEM		(LCD_MEM_7)
+#define LCD_SYMB_BATTERY_MEM			(LCD_MEM_7)
 #define LCD_UNIT_L1_FT_MEM			(LCD_MEM_5)
 #define LCD_UNIT_L1_K_MEM			(LCD_MEM_5)
 #define LCD_UNIT_L1_M_MEM			(LCD_MEM_7)
 #define LCD_UNIT_L1_I_MEM			(LCD_MEM_7)
-#define LCD_UNIT_L1_PER_S_MEM		(LCD_MEM_5)
-#define LCD_UNIT_L1_PER_H_MEM		(LCD_MEM_7)
-#define LCD_UNIT_L1_DEGREE_MEM		(LCD_MEM_5)
-#define LCD_UNIT_L2_KCAL_MEM		(LCD_MEM_7)
+#define LCD_UNIT_L1_PER_S_MEM			(LCD_MEM_5)
+#define LCD_UNIT_L1_PER_H_MEM			(LCD_MEM_7)
+#define LCD_UNIT_L1_DEGREE_MEM			(LCD_MEM_5)
+#define LCD_UNIT_L2_KCAL_MEM			(LCD_MEM_7)
 #define LCD_UNIT_L2_KM_MEM			(LCD_MEM_7)
 #define LCD_UNIT_L2_MI_MEM			(LCD_MEM_7)
 #define LCD_ICON_HEART_MEM			(LCD_MEM_2)
-#define LCD_ICON_STOPWATCH_MEM		(LCD_MEM_3)
+#define LCD_ICON_STOPWATCH_MEM			(LCD_MEM_3)
 #define LCD_ICON_RECORD_MEM			(LCD_MEM_1)
 #define LCD_ICON_ALARM_MEM			(LCD_MEM_4)
-#define LCD_ICON_BEEPER1_MEM		(LCD_MEM_5)
-#define LCD_ICON_BEEPER2_MEM		(LCD_MEM_6)
-#define LCD_ICON_BEEPER3_MEM		(LCD_MEM_7)
+#define LCD_ICON_BEEPER1_MEM			(LCD_MEM_5)
+#define LCD_ICON_BEEPER2_MEM			(LCD_MEM_6)
+#define LCD_ICON_BEEPER3_MEM			(LCD_MEM_7)
 
 // Bit masks for write access
 #define LCD_SEG_L1_0_MASK			(BIT2+BIT1+BIT0+BIT7+BIT6+BIT5+BIT4)
@@ -305,35 +305,35 @@ extern volatile s_display_flags display;
 #define LCD_SEG_L2_3_MASK			(BIT3+BIT2+BIT1+BIT0+BIT6+BIT5+BIT4)
 #define LCD_SEG_L2_4_MASK			(BIT3+BIT2+BIT1+BIT0+BIT6+BIT5+BIT4)
 #define LCD_SEG_L2_5_MASK			(BIT7)
-#define LCD_SEG_L2_COL1_MASK		(BIT4)
-#define LCD_SEG_L2_COL0_MASK		(BIT0)
+#define LCD_SEG_L2_COL1_MASK			(BIT4)
+#define LCD_SEG_L2_COL0_MASK			(BIT0)
 #define LCD_SEG_L2_DP_MASK			(BIT7)
 #define LCD_SYMB_AM_MASK			(BIT1+BIT0)
 #define LCD_SYMB_PM_MASK			(BIT0)
-#define LCD_SYMB_ARROW_UP_MASK		(BIT2)
-#define LCD_SYMB_ARROW_DOWN_MASK	(BIT3)
-#define LCD_SYMB_PERCENT_MASK		(BIT4)
+#define LCD_SYMB_ARROW_UP_MASK			(BIT2)
+#define LCD_SYMB_ARROW_DOWN_MASK		(BIT3)
+#define LCD_SYMB_PERCENT_MASK			(BIT4)
 #define LCD_SYMB_TOTAL_MASK			(BIT7)
-#define LCD_SYMB_AVERAGE_MASK		(BIT7)
+#define LCD_SYMB_AVERAGE_MASK			(BIT7)
 #define LCD_SYMB_MAX_MASK			(BIT7)
-#define LCD_SYMB_BATTERY_MASK		(BIT7)
+#define LCD_SYMB_BATTERY_MASK			(BIT7)
 #define LCD_UNIT_L1_FT_MASK			(BIT5)
 #define LCD_UNIT_L1_K_MASK			(BIT6)
 #define LCD_UNIT_L1_M_MASK			(BIT1)
 #define LCD_UNIT_L1_I_MASK			(BIT0)
-#define LCD_UNIT_L1_PER_S_MASK		(BIT7)
-#define LCD_UNIT_L1_PER_H_MASK		(BIT2)
-#define LCD_UNIT_L1_DEGREE_MASK		(BIT1)
-#define LCD_UNIT_L2_KCAL_MASK		(BIT4)
+#define LCD_UNIT_L1_PER_S_MASK			(BIT7)
+#define LCD_UNIT_L1_PER_H_MASK			(BIT2)
+#define LCD_UNIT_L1_DEGREE_MASK			(BIT1)
+#define LCD_UNIT_L2_KCAL_MASK			(BIT4)
 #define LCD_UNIT_L2_KM_MASK			(BIT5)
 #define LCD_UNIT_L2_MI_MASK			(BIT6)
 #define LCD_ICON_HEART_MASK			(BIT3)
-#define LCD_ICON_STOPWATCH_MASK		(BIT3)
-#define LCD_ICON_RECORD_MASK		(BIT7)
+#define LCD_ICON_STOPWATCH_MASK			(BIT3)
+#define LCD_ICON_RECORD_MASK			(BIT7)
 #define LCD_ICON_ALARM_MASK			(BIT3)
-#define LCD_ICON_BEEPER1_MASK		(BIT3)
-#define LCD_ICON_BEEPER2_MASK		(BIT3)
-#define LCD_ICON_BEEPER3_MASK		(BIT3)
+#define LCD_ICON_BEEPER1_MASK			(BIT3)
+#define LCD_ICON_BEEPER2_MASK			(BIT3)
+#define LCD_ICON_BEEPER3_MASK			(BIT3)
 
 
 
