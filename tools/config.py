33ec0466e4a3c1950702d9c0bd9a18a0bb61f659
Christiane Ruetten <cr@23bit.net>
2011-05-23 22:26:24 +0200

    cycle alarm help added

--- tools/config.py
+++ tools/config.py
@@ -188,7 +188,8 @@ DATA["CONFIG_STRENGTH"] = {
 DATA["CONFIG_CYCLE_ALARM"] = {
         "name": "Cycle Alarm (608 bytes)",
         "depends": [],
-        "default": True
+        "default": False,
+        "help": "Alarm clock for cycle-aware relative sleepers who want to sleep 4.5, 6, 7.5 (etc.) hours whenever they go to bed."
 }
 
 # not yet working
